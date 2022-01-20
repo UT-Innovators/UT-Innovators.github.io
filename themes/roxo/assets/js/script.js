3aea051182bbe8d992bc09c8d60ec4bf068f5264
UT-Innovators <nistak04@yahoo.co.jp>
2022-01-21 02:44:33 +0900

    integrate roxo

---
+++ themes/roxo/assets/js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  "use strict";
+
+  document.getElementById("totop").style.display="none";
+  // Scroll to top
+  $("a[href='#top']").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
+
+  // Smooth scroll
+  $('a.scroll-to').on('click', function (event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    }, 700);
+    event.preventDefault();
+  });
+
+  $('.site-testimonial-item').on('mouseenter', function(){
+    $('.site-testimonial-item').addClass('inactive');
+    $(this).removeClass('inactive').addClass('active');
+  });
+  $('.site-testimonial-item').on('mouseleave', function(){
+    $('.site-testimonial-item').removeClass('inactive');
+    $('.site-testimonial-item').removeClass('active');
+  });
+});
+
+$(window).on('scroll', function () {
+  var windscroll = $(window).scrollTop();
+  if (windscroll >= 100) {
+    $('.site-navigation').addClass('nav-bg');
+    document.getElementById("totop").style.display="block";
+  } else {
+    $('.site-navigation').removeClass('nav-bg');
+      document.getElementById("totop").style.display="none";
+  }
+});
